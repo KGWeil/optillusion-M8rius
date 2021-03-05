4859fb7c1e7c21111355be2441622013fe4c2603
Marius Klein <marius.j8klein@web.de>
2021-03-05 18:32:23 +0100

    Projekt abgeschlossen!

--- Wellen1.java
+++ Wellen1.java
@@ -12,14 +12,15 @@ import processing.core.PFont;
 public class Wellen1 extends PApplet
 {   int s; //erstelle Variable s  
     int r; //erstele Variable r
+    int aktuelleFarbe = 255; // Farbe 1. Quadrat weiß
     /**
      * settings() Methode 
      * Fenstergröße size(int width, int height) und smooth(int level) muss hier eingestellt werden.
      */
-    
+
     public void settings()
     {
-        size(500,500);
+        size(900,500);
 
     }        
 
@@ -31,12 +32,11 @@ public class Wellen1 extends PApplet
     @Override
     public void setup()
     {
-        background(0xFAA317);
-        // Zeichnen der optischen Täuschung
+        background(0xFAA317); //Hintergrundfarbe
+        s = 100; //Variable s hat nun diesen Wert
         alleQuadrate();
-        
-        s = 50; //Variable s hat nun diesen Wert
-        zeichneQuadrat(50,50,255,true);
+        linien();
+        farbwechsel(255); 
     }
 
     int farbwechsel (int farbe) {
@@ -49,7 +49,7 @@ public class Wellen1 extends PApplet
 
     /**
      * Zeichnet ein Quadrat der optischen Illusion
-     * @param    x=0   x-Koordinate Quadrat
+     * @param    x=   x-Koordinate Quadrat
      * @param    y=0   y-Koordinate Quadrat
      * @param    farbe    Füllfarbe des Quadrats
      * @param    links    Falls links true ist: Kreise links; falls links false ist: Kreise rechts
@@ -58,39 +58,36 @@ public class Wellen1 extends PApplet
     public void zeichneQuadrat(int x, int y, int farbe, boolean links)
     {
         ellipseMode(CORNER);  // Set ellipseMode is CORNER
-        fill(255);
+        fill(farbe);
         rect(x,y,s,s);
-        
+
         fill(farbwechsel(farbe)); 
 
-        int r = s/10; //Radius des 
-        if(links)
+        int r = s/10; //Radius des Kreises
+        if(links == true)
         {
             ellipse(x,y,2*r,2*r); //zeichnet Kreise links oben im Quadrat
-            ellipse(x,y+r*8,2*r,r*2); //zeichnet Kreise links unten im Quadrat
+            ellipse(x,y+r*8,2*r,2*r); //zeichnet Kreise links unten im Quadrat
         }  
         else
         {
-            ellipse(x+r*8,y,x+s,y+r*2);  //zeichnet Kreise rechts oben im Quadrat
-            ellipse(x+r*8,y+r*8,x+s,y+s); //zeichnet Kreise rechts unten im Quadrat
-
+            ellipse(x+r*8,y,2*r,2*r);  //zeichnet Kreise rechts oben im Quadrat
+            ellipse(x+r*8,y+r*8,2*r,2*r); //zeichnet Kreise rechts unten im Quadrat
         }  
     }
 
     public void alleQuadrate(){  
         noStroke();
-        int aktuelleFarbe = 255; // Farbe 1. Quadrat weiß
         boolean pos_aktuell = true; // Kreise zunächst links
-        for (int j=0; j<=3; j++) {
-            for (int i=0; i<=7; i++) {
-                zeichneQuadrat(i*s,0,aktuelleFarbe,true);
-                // Farbwechsel für nächstes Quadrat
-                if (aktuelleFarbe == 255) {
+        for (int j = 0; j <= 3; j++) {
+            for (int i = 0; i <= 7; i ++) {
+                zeichneQuadrat(2*r+i*s+s/2,j*s+s/2,aktuelleFarbe,pos_aktuell);
+                if (aktuelleFarbe == 255) { // Farbwechsel für nächstes Quadrat
                     aktuelleFarbe = 0;
                 } else {
                     aktuelleFarbe = 255;
                 }
-            } // end Zeichnen einer Reihe
+            } 
             // Farbwechsel für nächstes Quadrat
             if (aktuelleFarbe == 255) {
                 aktuelleFarbe = 0;
@@ -103,6 +100,14 @@ public class Wellen1 extends PApplet
             } else {
                 pos_aktuell = true;
             }
+        }
+    }
+
+    public void linien(){ //zeichnet 3 Lininen zwischen Quadraten
+        stroke(0,255,0);
+        strokeWeight(4);
+        for (int i=1; i<=3; i++) {
+            line(s/2-5,s*i+s/2,s/2+8*s+5,s*i+s/2); 
         }
     }
 
